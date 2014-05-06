018b754c7795fe3e6394a645c3876cf552df4a86
Ian Li <ianarli@gmail.com>
2014-05-06 00:58:50 -0700

    Added JS for tools page

---
+++ js/tools.js
@@ -0,0 +1,79 @@
+(function (window, $, undefined) {
+
+  var tagRegex = /\btag-\S+\b/g;
+
+  function getTagsList() {
+    // Cycle through all the tool items and generate a hash of tags.
+    var tagsHash = {};
+    $('.tools-item').each(function () {
+      var $this = $(this),
+          classStr = $this.attr('class'),
+          tags = classStr.match(tagRegex);
+
+      if (tags == null) {
+        return;
+      }
+
+      $.each(tags, function (index, tag) {
+        if (typeof tagsHash[tag] === 'undefined') {
+          tagsHash[tag] = tag.replace('tag-', '').replace('-', ' ');
+        }
+      });
+    });
+
+    // Generate a list of tags from the hash of tags.
+    var tagsList = $.map(tagsHash, function (value, key) {
+                      return {
+                        id: key,
+                        value: value
+                      };
+                    });
+
+    tagsList.sort(function (a, b) {
+      if (a.id > b.id) {
+        return 1;
+      } else if (a.id < b.id) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+
+    return tagsList;
+  }
+
+  $(document).ready(function () {
+    // Create Isotope layout.
+    $('#tools-frame').isotope({
+      itemSelector: '.tools-item',
+      layoutMode: 'fitRows',
+      sortBy: 'created_at',
+      sortAscending: false
+    });
+
+    // Create tags list.
+    $.templates("#tmpl-tags-list")
+      .link(
+        '#tools-sidebar',
+        {
+          tags: getTagsList()
+        }
+      );
+
+    // Handle clicks on the tag list.
+    var $currentActive = $('#tools-sidebar a[data-filter].active');
+    $('#tools-sidebar a[data-filter]').click(function () {
+      var $this = $(this),
+          filterValue = $this.data('filter');
+
+      $currentActive.removeClass('active');
+
+      $currentActive = $this.addClass('active');
+
+      $('#tools-frame').isotope({ filter: filterValue });
+
+      return false;
+    });
+  });
+
+}(window, jQuery));
